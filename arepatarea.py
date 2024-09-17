d179d1ea3b4f38d41e14cab9731e4b9e305ebe16
JandroT <alejandro.sequera@correo.unimet.edu.ve>
2024-09-16 21:37:23 -0400

    Add files via upload

---
+++ arepatarea.py
@@ -0,0 +1,19 @@
+print ("Bienvenido a la cocina virtual!, hoy realizaremos arepas")
+harina = int(input("Porfavor ingresa la cantidad en gramos de harina con la que trabajarás:"))
+print ("Para la cantidad de harina que utilizaras vas a necesitar los siguientes elementos")
+agua = int(harina * 1)
+sal = int(harina / 16 / 3)
+aceite = int(harina / 16)
+print(agua, "ml de agua, ", sal, "gramos de sal y ", aceite, "ml de aceite.")
+print("ahora coloca la harina, el agua y la sal en un bowl y mezclalo")
+bowl = agua + sal +  + harina
+print("El bowl deberia tener ", bowl, " gramos de masa")
+print("Forma las arepas con tus manos y luego en un sarten pon el aceite para luego poner las arepas y cocinarlas")
+print("Debes darle vueltas a la arepa para que se cocine por ambos lados segun tu gusto")
+sarten = bowl + aceite
+print("al colocar las arepas en el sarten este tendra un peso de ", bowl, " gramos")
+arepas = sarten * 0.85
+print("pero finalmente al tener tus arepas listas estas tendran una masa aproximada de ", int(arepas), " gramos, debido a la evaporacion")
+print("!Disfruta de tus arepas¡")
+
+#BY ALEJANDRO SEQUERA UNIVERSIDAD METROPOLITANA
